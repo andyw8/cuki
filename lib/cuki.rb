4105298312fc50bf864039c98b9654ad4cb03f19
Andy Waite <andy@andywaite.com>
2011-10-12 21:58:14 +0100

    Version 0.0.2

--- lib/cuki.rb
+++ lib/cuki.rb
@@ -10,78 +10,96 @@ class Cuki
   START_INDICATOR = /\d\. \*Specification\*/
 
   def self.invoke
+    new
+  end
 
+  def initialize
+    read_config
+    configure_http_client
+    @mappings.each { |key, value|  process_mapping key, value }    
+    autoformat
+  end
+  
+  private
+  
+  def read_config
     config_path = 'config/cuki.yaml'
 
     config = YAML::load( File.open( config_path ) )
 
-    base = config["base"]
-    raise "base not found in #{config_path}" unless base
+    @base = config["base"]
+    raise "base not found in #{config_path}" unless @base
 
-    mappings = config["mappings"]
-    raise "mappings not found in #{config_path}" unless mappings
+    @mappings = config["mappings"]
+    raise "mappings not found in #{config_path}" unless @mappings
+    
+    @draft_indicator = config['draft']
+  end
+  
+  def configure_http_client
+    @client = HTTPClient.new
+    @client.ssl_config.set_trust_ca(ENV['CER']) if ENV['CER']
+    @client.ssl_config.set_client_cert_file(ENV['PEM'], ENV['PEM']) if ENV['PEM']
+  end
+  
+  def process_mapping(key, value)
+    wiki_link = @base + key.to_s
 
-    client = HTTPClient.new
-    client.ssl_config.set_trust_ca(ENV['CER']) if ENV['CER']
-    client.ssl_config.set_client_cert_file(ENV['PEM'], ENV['PEM']) if ENV['PEM']
+    puts "Downloading #{wiki_link}"
 
-    mappings.each do |key, value|
-      wiki_link = base + key.to_s
+    response = @client.get wiki_link
 
-      puts "Downloading #{wiki_link}"
+    doc = Nokogiri(response.body)
 
-      response = client.get wiki_link
+    title = doc.css('title').text
 
-      doc = Nokogiri(response.body)
+    wiki_text = CGI.unescapeHTML(doc.css('#markupTextarea').text)
+    wiki_text.gsub('&nbsp;', '')
 
-      title = doc.css('title').text
+    cuke_text = ''
+    cuke_text += "@pending\n" if wiki_text.include?(@draft_indicator)
 
-      wiki_text = CGI.unescapeHTML(doc.css('#markupTextarea').text)
-      wiki_text.gsub('&nbsp;', '')
+    title = title.split(' - ').first
 
-      cuke_text = ''
-      cuke_text += "@pending\n" if wiki_text.include?(config['draft'])
+    # assuming that title is format REF - TITLE - PROJECT NAME - WIKI NAME
+    cuke_text += "Feature: " + title + "\n\n"
 
-      # assuming that title is format REF - TITLE - PROJECT NAME - WIKI NAME
-      cuke_text += "Feature: " + title.split(' - ').first + "\n\n"
+    cuke_text += "#{wiki_link}\n\n"
 
-      cuke_text += "#{wiki_link}\n\n"
+    raise "couldn't find start of acceptance criteria in #{title}" unless wiki_text.match(START_INDICATOR)
 
-      raise "couldn't find start of acceptance criteria" unless wiki_text.match(START_INDICATOR)
+    cuke_text += wiki_text.split(START_INDICATOR).last
 
-      cuke_text += wiki_text.split(START_INDICATOR).last
+    # remove the double pipes used for table headers in Confluence
+    cuke_text.gsub!('||', '|')
 
-      # remove the double pipes used for table headers in Confluence
-      cuke_text.gsub!('||', '|')
+    # remove other noise
+    cuke_text.gsub!('\\', '')
 
-      # remove other noise
-      cuke_text.gsub!('\\', '')
+    # remove any unwanted headers
+    cuke_text.gsub!(/h\d\. /, '')
 
-      # remove any unwanted headers
-      cuke_text.gsub!(/h\d\. /, '')
+    # remove an other confluence markup
+    cuke_text.gsub!(/\{.*\}/, '')
 
-      # remove an other confluence markup
-      cuke_text.gsub!(/\{.*\}/, '')
+    # check features folder exists
+    raise "features folder not found" unless File.exists?('features')
 
-      # check features folder exists
-      raise "features folder not found" unless File.exists?('features')
+    file_path = "features/#{value}.feature"
+    dir_path = File.dirname(file_path)
 
-      file_path = "features/#{value}.feature"
-      dir_path = File.dirname(file_path)
-
-      unless File.exists?(dir_path)
-        Dir.mkdir(dir_path)
-      end
-
-      File.open(file_path, 'w') do |f|
-        puts "Writing #{file_path}"
-        f.puts cuke_text
-      end
-
+    unless File.exists?(dir_path)
+      Dir.mkdir(dir_path)
     end
 
-    # clean up with Cucumber's autoformatter
-    `cucumber -a . --dry-run -P` unless ENV['SKIP_AUTOFORMAT']
-
+    File.open(file_path, 'w') do |f|
+      puts "Writing #{file_path}"
+      f.puts cuke_text
+    end
   end
+  
+  def autoformat
+    `cucumber -a . --dry-run -P` unless ENV['SKIP_AUTOFORMAT']
+  end
+  
 end
