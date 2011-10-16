ab3d260135afea2f4e204d67ca02e6461b84d2ce
Andy Waite <andy@andywaite.com>
2011-10-16 14:48:57 +0100

    Configuring Aruba

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,8 @@
+#ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
+
 require 'aruba/cucumber'
 require 'webmock/cucumber'
 
-ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
+Before do
+  @dirs = ["."]
+end
