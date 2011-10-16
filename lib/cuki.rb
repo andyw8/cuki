8540579bffa81efdb29865c71bf655a02c1e43e3
Andy Waite <andy@andywaite.com>
2011-10-16 20:46:02 +0100

    Move pusher stuff into separate class

--- lib/cuki.rb
+++ lib/cuki.rb
@@ -54,15 +54,7 @@ class Cuki
       end
       #autoformat
     elsif 'push' == action
-      feature_to_be_pushed = args[1] # e.g. features/products/add_product.feature
-      feature_as_in_yaml = feature_to_be_pushed.gsub('features/', '').gsub('.feature', '')
-      id = @config['mappings'].invert[feature_as_in_yaml]
-      raise "No mapping found for #{feature_as_in_yaml}" unless id
-
-      content = File.open(feature_to_be_pushed).read.gsub(/Feature: .*/, '')
-      title = File.open(feature_to_be_pushed).read.match(/Feature: (.*)/)[1]
-      response = @client.post(@config['host'], {:title => title, :content => content})
-
+      Pusher.push(args[1], @config)
     else
       puts "Unknown action '#{action}"
       exit(1)
